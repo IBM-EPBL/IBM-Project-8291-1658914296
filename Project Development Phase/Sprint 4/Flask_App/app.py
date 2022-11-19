67fa49dd0b00ac695ecdb83e1d58ec097f766eb0
gowtham22-crown <66377279+gowtham22-crown@users.noreply.github.com>
2022-11-19 16:26:43 +0530

    Final

--- Project Development Phase/Sprint 4/Flask_App/app.py
+++ Project Development Phase/Sprint 4/Flask_App/app.py
@@ -36,22 +36,35 @@ def information():
 @app.route('/predict', methods=['GET', 'POST']) 
 def upload():
     if request.method == 'POST':
-        f=request.files['file'] #requesting the file
-        basepath=os.path.dirname(__file__)#storing the file directory 
-        filepath=os.path.join(basepath, "uploads", f.filename) #storing the file in uploads folder
-        f.save(filepath) #saving the file
+        try:
+            f=request.files['file'] #requesting the file
+            basepath=os.path.dirname(__file__)#storing the file directory 
+            filepath=os.path.join(basepath, "uploads", f.filename) #storing the file in uploads folder
+            f.save(filepath) #saving the file
+        except Exception as e:
+            print("EXCEPTION AT ACCESSING THE FILE : ", e.message)
+            return "PLEASE UPLOAD AN IMAGE FILE"
+
         #Loading the saved model 
         print("[INFO] loading model...")
-        model = pickle.loads (open('parkinson.pkl', "rb").read())
-        # pre-process the image in the same manner we did earlier 
-        image = cv2.imread(filepath)
-        output = image.copy()
-        # load the input image, convert it to grayscale, and resize 
-        output = cv2.resize(output, (128, 128))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        image = cv2.resize(image, (200, 200))
-        image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU) [1]
+        try:
+            model = pickle.loads (open('parkinson.pkl', "rb").read())
+        except Exception as e:
+            print("EXCEPTION WHILE LOADING THE MODEL : ", e.message)
+            return str(e.message)
         
+        try:
+            # pre-process the image in the same manner we did earlier 
+            image = cv2.imread(filepath)
+            output = image.copy()
+            # load the input image, convert it to grayscale, and resize 
+            output = cv2.resize(output, (128, 128))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            image = cv2.resize(image, (200, 200))
+            image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU) [1]
+        except Exception as e:
+            print("EXCEPTION WHILE PERFORMING OPENCV OPERATION : ", e.message)
+            return str(e.message)
         
         # quantify the image and make predictions based on the extracted
         # features using the last trained Random Forest
